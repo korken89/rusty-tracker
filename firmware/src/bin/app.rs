97ced0c68acb5cae555f7f2e420c6b7f0ee84e80
Emil Fresk <emil.fresk@gmail.com>
2023-03-17 22:24:27 +0100

    Systick working

--- firmware/src/bin/app.rs
+++ firmware/src/bin/app.rs
@@ -4,17 +4,19 @@
 #![feature(async_fn_in_trait)]
 #![allow(incomplete_features)]
 
+use fugit::MicrosDurationU32;
+use rtic_monotonics::{systick::Systick, Monotonic};
 use rusty_tracker as _; // global logger + panicking-behavior + memory layout
 
-// defmt::timestamp!("{=u64:us}", {
-//     let time_us: dwm1001_async::rtc_monotonic::fugit::MicrosDurationU64 =
-//         app::monotonics::now().duration_since_epoch().convert();
-//
-//     time_us.ticks()
-// });
+defmt::timestamp!("{=u64:us}", {
+    let time_us: MicrosDurationU32 = Systick::now().duration_since_epoch().convert();
+
+    time_us.ticks() as u64
+});
 
 #[rtic::app(device = rusty_tracker::hal::pac, dispatchers = [SWI0_EGU0])]
 mod app {
+    use rtic_monotonics::systick::*;
     use rusty_tracker::bsp;
 
     #[shared]
@@ -25,10 +27,22 @@ mod app {
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
-        defmt::info!("init");
+        defmt::println!("pre init");
 
         let () = bsp::init(cx.core, cx.device);
 
+        defmt::println!("init");
+
+        task1::spawn().ok();
+
         (Shared {}, Local {})
+    }
+
+    #[task]
+    async fn task1(_: task1::Context) {
+        loop {
+            defmt::info!("hello");
+            Systick::delay(100.millis()).await;
+        }
     }
 }
--- firmware/src/bsp.rs
+++ firmware/src/bsp.rs
@@ -1,17 +1,21 @@
 use crate::hal::{
-    clocks::{Clocks, LfOscConfiguration},
+    clocks::{Clocks, LfOscConfiguration, HFCLK_FREQ},
     gpio::p0,
     pac,
 };
+use rtic_monotonics::systick::Systick;
 
 #[inline(always)]
 pub fn init(c: cortex_m::Peripherals, p: pac::Peripherals) -> () {
-    let _clocks = Clocks::new(p.CLOCK)
+    let clocks = Clocks::new(p.CLOCK)
         .enable_ext_hfosc()
         .set_lfclk_src_external(LfOscConfiguration::NoExternalNoBypass)
         .start_lfclk();
 
     let port0 = p0::Parts::new(p.P0);
+
+    let systick_token = rtic_monotonics::make_systick_handler!();
+    Systick::start(c.SYST, HFCLK_FREQ, systick_token);
 
     ()
 }
